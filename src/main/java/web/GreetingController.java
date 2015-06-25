d29ceb4144bc7258291b71aeb81467855bb9b642
antonnaumoff <antonnaumoff@gmail.com>
2015-06-25 14:17:34 +0300

    Angular REST practices 1.1

--- src/main/java/web/GreetingController.java
+++ src/main/java/web/GreetingController.java
@@ -1,21 +1,25 @@
 package web;
 
-import models.Greeting;
+import models.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
+import service.DataService;
+import utils.exceptions.DataBaseException;
 
 @RestController
+@RequestMapping("/rest/departments")
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    private DataService dataService;
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Department greeting(@PathVariable String id) throws DataBaseException {
+        int iD = Integer.parseInt("id");
+        return dataService.getDepartmentById(iD);
+
     }
 }
