954ba4c820b23096b14a24841e759fbf19078cab
antonnaumoff <antonnaumoff@gmail.com>
2015-07-22 11:03:09 +0300

    Require.js practice

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -1,15 +1,19 @@
 require.config({
     baseUrl: "scripts",
     paths: {
-        jQuery: "lib/jquery-1.11.3",
-        util: "lib/util"
+
+        jQuery: 'lib/jquery-1.11.3',
+        util: 'lib/util'
+
+
     }
 });
 
 
+
 require(["jQuery"], function (jQuery) {
-    console.log("Succesfull load")
+    console.log("Succesfull load jQuery")
 });
-require(["util"], function (jQuery) {
-    console.log("Succesfull load")
+require(["util"], function (util) {
+    console.log("Succesfull load util")
 });
